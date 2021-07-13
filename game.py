a2db72b42e5bf8d642a89e738e7c3a58e15bae77
nyugraceng <87045641+nyugraceng@users.noreply.github.com>
2021-07-13 17:35:39 -0400

    Validating user inputs

--- game.py
+++ game.py
@@ -8,5 +8,15 @@ print("Rock, Paper, Scissors, Shoot!")
 x = input("Please choose one of 'rock', 'paper', 'scissors'")
 print(x)
 
+if (x == "rock") or (x == "paper") or (x =="scissors"):
+    print("VALID")  
+else:
+    print("OOPS, INVALID, PLEASE TRY AGAIN")
+    exit()
+
+print("LATER MESSAGES")
+
+
+
 
 
