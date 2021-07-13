d849c4c486c7ae78cc9ab3ed177eac08941144f5
nyugraceng <87045641+nyugraceng@users.noreply.github.com>
2021-07-13 17:15:08 -0400

    Set up the repo

---
+++ game.py
@@ -0,0 +1,12 @@
+print("Rock, Paper, Scissors, Shoot!")
+
+# ASK FOR A USER INPUT
+# VALIDATE USE INPUT
+# GENERATE A COMPUTER CHOICE
+# DETERMINE THE WINNER
+
+x = input("Please choose one of 'rock', 'paper', 'scissors'")
+print(x)
+
+
+
